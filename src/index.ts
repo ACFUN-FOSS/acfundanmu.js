885b07d434c9330df2ba15ad18db9082615910f2
qq617564112 <617564112@qq.com>
2025-11-30 00:08:18 +0800

    refactor: 移除 ApiServer 及相关引用，删除 createServer 与服务器配置
    - 删除 src/server/ApiServer.ts
    - 更新 src/index.ts 移除 ApiServer 导入/导出
    - 移除服务器配置与 createServer 工厂函数

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { AcFunLiveApi } from './AcFunLiveApi';
-import { ApiServer } from './server/ApiServer';
-import { ApiConfig, ServerConfig } from './types';
+import { ApiConfig } from './types';
 
 // 默认配置
 const defaultApiConfig: ApiConfig = {
@@ -13,18 +12,9 @@ const defaultApiConfig: ApiConfig = {
   }
 };
 
-const defaultServerConfig: ServerConfig = {
-  port: 3000,
-  host: 'localhost',
-  corsOrigin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-  rateLimit: {
-    windowMs: 15 * 60 * 1000, // 15分钟
-    max: 100 // 限制每个IP每15分钟最多100个请求
-  }
-};
 
 // 导出主要类
-export { AcFunLiveApi, ApiServer };
+export { AcFunLiveApi };
 
 // 导出类型
 export * from './types';
@@ -35,14 +25,6 @@ export function createApi(config?: Partial<ApiConfig>): AcFunLiveApi {
   return new AcFunLiveApi(finalConfig);
 }
 
-// 创建服务器实例的工厂函数
-export function createServer(apiConfig?: Partial<ApiConfig>, serverConfig?: Partial<ServerConfig>): ApiServer {
-  const finalApiConfig = { ...defaultApiConfig, ...apiConfig };
-  const finalServerConfig = { ...defaultServerConfig, ...serverConfig };
-  
-  const api = new AcFunLiveApi(finalApiConfig);
-  return new ApiServer(finalServerConfig, api);
-}
 
 // 默认导出
 export default AcFunLiveApi;
